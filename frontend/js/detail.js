b67eaf1785760995b29772417561a595c9ad6d16
loay ashraf <117324979+loay160@users.noreply.github.com>
2024-07-24 05:10:36 +0300

    add js
    js for cart & product

---
+++ frontend/js/detail.js
@@ -0,0 +1,53 @@
+import products from "./products.js";
+import cart from "./cart.js";
+let app = document.getElementById('app');
+let temporaryContent = document.getElementById('temporaryContent');
+
+// load template file
+const loadTemplate = () => {
+    fetch('./index0.html')
+        .then(response => response.text())
+        .then(html => {
+            app.innerHTML = html;
+            let contentTab = document.getElementById('contentTab');
+            contentTab.innerHTML = temporaryContent.innerHTML;
+            temporaryContent.innerHTML = null;
+            cart();
+            initApp();
+        })
+}
+loadTemplate();
+const initApp = () => {
+	let idProduct = new URLSearchParams(window.location.search).get('id');
+	let info = products.filter((value) => value.id == idProduct)[0];
+	
+	if(!info){
+        window.location.href= './index1.html';
+	}
+	let detail = document.querySelector('.detail');
+	detail.querySelector('.image img').src = info.image;
+	detail.querySelector('.name').innerText = info.name;
+	detail.querySelector('.price').innerText = '$' + info.price;
+	detail.querySelector('.description').innerText = info.description;
+	detail.querySelector('.addCart').dataset.id = idProduct;
+}
+// similar producr
+let listProduct = document.querySelector('.listProduct');
+    listProduct.innerHTML = null;
+    products.filter((value) => value.id != products).forEach(product => {
+        let newProduct = document.createElement('div');
+        newProduct.classList.add('item');
+        newProduct.innerHTML =
+        `
+            <a href="/detail.html?id=${product.id}">
+	    	    <img src="${product.image}"/>
+	        </a>
+		    <H2>${product.name}</h2>
+		    <div class="price">$${product.price}</div>
+		    <button class="addCart"
+                data-id="${product.id}">
+			        Add To Cart
+		    </button>
+	    `;
+        listProduct.appendChild(newProduct);
+    })
---
+++ frontend/js/products.js
@@ -0,0 +1,59 @@
+const products = [
+	{
+		"id":1,
+		"name":"LD01 LOUNGHE CHAIR",
+		"price":200,
+		"description": "Expertly rendered by Carl Hansen & Son, the  lounge chair-first",
+		"image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTat9CailGq0bmuYRkDSUPPwaH9LPSzrGFktyi7lpDevftyytIiympYtAvuEwkudSlOodM&usqp=CAU"
+	},
+	{
+		"id":2,
+		"name":"LD02 LOUNGHE CHAIR",
+		"price":300,
+		"description": "Expertly rendered by Carl Hansen & Son, the  lounge chair-first",
+		"image": "img/2.webp"
+	},
+	{
+		"id":3,
+		"name":"LD03 LOUNGHE CHAIR",
+		"price":250,
+		"description": "Expertly rendered by Carl Hansen & Son, the  lounge chair-first",
+		"image": "img/5.jpg"
+	},
+	{
+		"id":4,
+		"name":"LD04 LOUNGHE CHAIR",
+		"price":350,
+		"description": "Expertly rendered by Carl Hansen & Son, the  lounge chair-first",
+		"image": "img/2.webp"
+	},
+	{
+		"id":5,
+		"name":"LD05 LOUNGHE CHAIR",
+		"price":400,
+		"description": "Expertly rendered by Carl Hansen & Son, the  lounge chair-first",
+		"image": "img/5.jpg"
+	},
+	{
+		"id":6,
+		"name":"LD06 LOUNGHE CHAIR",
+		"price":450,
+		"description": "Expertly rendered by Carl Hansen & Son, the  lounge chair-first",
+		"image": "img/2.webp"
+	},
+	{
+		"id":5,
+		"name":"LD05 LOUNGHE CHAIR",
+		"price":400,
+		"description": "Expertly rendered by Carl Hansen & Son, the  lounge chair-first",
+		"image": "img/5.jpg"
+	},
+	{
+		"id":4,
+		"name":"LD04 LOUNGHE CHAIR",
+		"price":350,
+		"description": "Expertly rendered by Carl Hansen & Son, the  lounge chair-first",
+		"image": "img/2.webp"
+	}
+];
+export default products
